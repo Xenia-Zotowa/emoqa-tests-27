4cc95858fd31c57c67fb42adbc68e927372b6b21
Xenia Zotova <ksenias.zotova@gmail.com>
2024-04-20 14:41:49 +0300

    add SimpleJUnitTest

---
+++ src/test/java/SimpleJUnitTest.java
@@ -0,0 +1,22 @@
+
+import org.junit.Test;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SimpleJUnitTest {
+
+    @Test
+    private void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
+}
