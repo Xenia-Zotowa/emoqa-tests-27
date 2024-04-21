8bc0a5235ad73c7c33fdd121d9fc617a1762ab8c
Xenia Zotova <ksenias.zotova@gmail.com>
2024-04-21 10:27:54 +0300

    add SimpleJUnitTest - Chenges2

--- src/test/java/SimpleJUnitTest.java
+++ src/test/java/SimpleJUnitTest.java
@@ -1,14 +1,52 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+
 
 public class SimpleJUnitTest {
+
+    int result;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("\n###  beforeAll()\n");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+      System.out.println("###   beforeEach()");
+      result = getResult();
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("###   afterEach()\n");
+        result = 0;
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("\n###  afterAll()\n");
+    }
+
     @Test
-    void test(){
-        open("https://www.google.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+    void firstTest(){
+        System.out.println("###    firstTest()");
+        Assertions.assertTrue(result > 2);
+    }
+
+    @Test
+    void secondTest(){
+        System.out.println("###    secondTest()");
+        Assertions.assertTrue(result > 2);
+    }
+
+    @Test
+    void thirdTest(){
+        System.out.println("###    thirdTest()");
+        Assertions.assertTrue(result > 2);
+    }
+
+    private int getResult() {
+        return 3;
     }
 }
